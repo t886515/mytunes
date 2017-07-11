83f0e69b46032178b9bdfcedcdabccf4c51c1ddf
Hack Reactor Student <student@hackreactor.com>
2017-07-11 11:28:35 -0500

    SongQueueView now visisble and dequeues songs

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,8 +1,18 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
-  
+
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  events: {
+    'click': function() {
+      this.model.dequeue();
+    }
+  },
+
   render: function() {
-    return this.$el;
+    return this.$el.html(this.template(this.model.attributes));
   }
+
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,13 +1,20 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+  
   initialize: function() {
+    this.render();
     this.collection.on('add', this.render, this);
     this.collection.on('remove', this.render, this);
   },
 
   render: function() {
     //call the render function from song queue entry view
+    this.$el.children().detach();
+    
+    this.$el.html('<th>Song Queue</th>');
+    
     this.collection.forEach(function(song) {
       var newSongQueueEntryView = new SongQueueEntryView({model: song});
       this.$el.append(newSongQueueEntryView.render());
