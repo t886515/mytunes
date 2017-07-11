e63610b93c900a69541adc6c9f7822df2f814336
Hack Reactor Student <student@hackreactor.com>
2017-07-11 16:08:25 -0500

    Passes almost all tests

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -3,33 +3,29 @@ var Songs = Backbone.Collection.extend({
 
   model: SongModel,
   url: 'http://parse.atx.hackreactor.com/mytunes/classes/songs',
+  parse: function(response) {
+    return response.results;
+  },
   
   initialize: function() {
+    // this.retrieve();
+    this.fetch();
+  },
+  
+  retrieve: function() {
     var that = this;
-    this.fetch({
-      success: function(collection, response) {
-        var array = collection.parse(response);
-        _.each(array, function(song) {
-          that.add(song);
-        }); 
+    $.ajax({
+      url: 'http://parse.atx.hackreactor.com/mytunes/classes/songs',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        //console.log(data, 'show us the data');
+        that.add(data.results);
+      },
+      error: function(data) {
+        console.error('we did not receive anything :(');
       }
     });
-
-    console.log(this);
-  }
-  
-  // fetch: function() {
-  //   // $.ajax({
-  //   //   url: 'http://parse.atx.hackreactor.com/mytunes/classes/songs',
-  //   //   type: 'GET',
-  //   //   contentType: 'application/json',
-  //   //   success: function(data) {
-  //   //     console.log(data, 'show us the data');
-  //   //   },
-  //   //   error: function(data) {
-  //   //     console.error('we did not receive anything :(');
-  //   //   }
-  //   // });
-  // },
+  },
 
 });
